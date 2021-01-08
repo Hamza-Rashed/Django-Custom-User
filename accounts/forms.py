08e4a3fa4abaf4c5308aef51520be05caf7fb27c
Hamza Rashed <hamzarashed503@gmail.com>
2021-01-08 11:01:29 -0800

    done

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,14 @@
 from django import forms
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm
 
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
